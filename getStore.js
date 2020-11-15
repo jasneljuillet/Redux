5fda7ef15de168b215cd71876c1e99c6cf4dcca1
Jasnel <jasneljuillet1995@gmail.com>
2020-11-15 11:11:55 -0500

    Get State from the Redux StorePassed

---
+++ getStore.js
@@ -0,0 +1,11 @@
+
+const Redux = require('redux');
+
+const store = Redux.createStore(
+    (state = 5) => state
+  );
+  
+  //  Get State from the Redux StorePassed
+  const currentState = store.getState()
+  console.log(currentState) //5
+  
