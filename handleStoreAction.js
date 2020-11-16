dff7e2219780ba4e7e48fa23657c6ede1d1932fb
jasneljuillet <jasneljuillet1995@gmail.com>
2020-11-16 08:44:50 -0500

    Handle an Action in the StorePassed

---
+++ handleStoreAction.js
@@ -0,0 +1,18 @@
+const Redux = require("redux");
+
+const defaultState = {
+  login: false,
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
