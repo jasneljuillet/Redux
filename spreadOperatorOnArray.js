b87d66474338b8f7907354f05b545610a290533a
jasneljuillet <jasneljuillet1995@gmail.com>
2020-12-04 14:26:43 -0500

    Return a copy to existing Array

---
+++ spreadOperatorOnArray.js
@@ -0,0 +1,21 @@
+const Redux = require("redux");
+
+const immutableReducer = (state = ["Do not mutate state!"], action) => {
+  switch (action.type) {
+    case "ADD_TO_DO":
+      // Don't mutate state here or the tests will fail
+      [...state, action.todo];
+      return;
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: "ADD_TO_DO",
+    todo,
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
